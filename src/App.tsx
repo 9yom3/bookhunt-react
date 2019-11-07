909aa3d3a16558591fca4a51cc5e5074eeaacbb0
Guillaume Bailleul <laibulle@gmail.com>
2019-11-07 21:35:00 +0100

    chore: create init state

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import LoginScreen from './screens/LoginScreen'
 import {
-  Switch,
-  Route,
-  Redirect,
   withRouter,
   RouteComponentProps
 } from "react-router-dom";
 
 import './App.scss';
-import CopiesScreen from './screens/CopiesScreen';
-import { LoginMutationResponse } from './graphql/__generated__/LoginMutation.graphql';
 
 type Props = {
 }
 
 const App: React.FC<RouteComponentProps<Props>> = (props: RouteComponentProps<Props>) => {
-  const [loginRes, setLoginRes] = useState<LoginMutationResponse | null>(null)
-
-  const onLogin = (res: LoginMutationResponse) => {
-    setLoginRes(res)
-    props.history.push('/copies')
-  }
 
   return (
-    <div className="container">
-      <Switch>
-        <Route exact path="/">
-          <Redirect from="/" to="/login" />
-        </Route>
-        <Route path="/login">
-          <LoginScreen onLogin={onLogin} />
-        </Route>
-        <Route path="/copies">
-          {loginRes ? <CopiesScreen user={loginRes} /> : <Redirect from="/" to="/login" />}
-        </Route>
-      </Switch>
+    <div>
+      Hello World !
     </div>
 
   )
--- src/component/Hello.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-type Props = {
-    username: string
-}
-export default (props: Props) => <div className="hello">Hello {props.username}!</div>
